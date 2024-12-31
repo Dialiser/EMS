8b7aa3e983059b4f9b62d3d182dfd81d2a512586
SxtyxmBhxrdwxj <111901770+Dialiser@users.noreply.github.com>
2024-12-31 19:48:40 +0530

    Update FilterEmployee.js

--- management/src/components/FilterEmployee.js
+++ management/src/components/FilterEmployee.js
@@ -1,16 +1,24 @@
-import React from 'react';
+ import React from 'react';
 
 const FilterEmployee = ({ filters, onFilterChange }) => {
   return (
-    <div>
-      <h3>Filter Employees</h3>
+    <div
+      style={{
+        margin: '20px 0',
+        padding: '10px',
+        border: '1px solid black',
+      }}
+    >
+      <h3 style={{ marginBottom: '15px' }}>Filter Employees</h3>
       <input
+        style={{ display: 'block', marginBottom: '10px', width: '100%' }}
         type="text"
         placeholder="Filter by Name"
         value={filters.name}
         onChange={(e) => onFilterChange('name', e.target.value)}
       />
       <input
+        style={{ display: 'block', marginBottom: '10px', width: '100%' }}
         type="text"
         placeholder="Filter by Department"
         value={filters.department}
@@ -21,3 +29,4 @@ const FilterEmployee = ({ filters, onFilterChange }) => {
 };
 
 export default FilterEmployee;
+
